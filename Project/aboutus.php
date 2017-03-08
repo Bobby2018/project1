23e49a6c61c185e3c4c4766bb61dffced20336bc
Bobby2018 <bobby20@comcast.net>
2017-03-07 18:57:41 -0700

    Add files via upload

---
+++ Project/aboutus.php
@@ -0,0 +1,38 @@
+<?php
+include 'header.php';
+include 'nav.php';
+?>
+
+<body>
+		
+	<!-- create three coulumn with lg and md size-->
+		
+		<div class="container-fluid">
+			<div class="row visible-on">
+                            <div class="col-md-4">
+                                <img src="Bobby.jpg" class="img" alt="Bobby" width="auto" height="auto"></img>
+                            </div>
+                    <div class="col-md-4 col-lg-5">
+                            <div class="Title">
+                                <h1>About US!</h1> 
+                                <h4>Bobby Elliott and Jack McCoy</h4> 
+                            </div>
+                        <div class="maincontent">    
+                            <p> Both Bobby and Jack are currently attending Colorado State University. Bobby Elliott(on the left) is from Steamboat Springs, CO. He is double majoring in Applied Computing Technologies and Finance. Growing up in Steamboat he came to love skiing and recently switched from alpine to telemark skiing. </p>
+                            <p> Jack is from...</p>
+                        </div>	
+                </div>
+                          
+            
+                          <div class="col-md-4 col-lg-3">
+                          <img src="jack.jpg" class="img" alt="Jack" width="320" height="320"></img>
+                          </div>
+                       </div>   
+                     </div>     
+                          
+<?php
+include 'footer.php';
+?>
+
+	</body>
+</html>
---
+++ Project/index.php
@@ -0,0 +1,44 @@
+<?php
+include 'header.php';
+include 'nav.php';
+?>
+	<body>
+		
+	<!-- create three coulumn with lg and md size-->
+		
+		<div class="container-fluid">
+			<div class="row visible-on">
+                            <div class="col-md-4">
+                                <img src="tumeric2.jpg" class="img" alt="tumeric" width="250" height="200"></img>
+                                <img src="kohlrabi.jpg" class="img" alt="kohlrabi" width="250" height="200"></img>
+                                <img src="cilantro.jpg" class="img" alt="cilantro" width="250" height="200"></img>
+                                <p> Images from <a href=https://draxe.com/cilantro-benefits/>draxe.com</a> , <a href=http://www.simplyrecipes.com/kohlrabi/>simplyrecipes</a> , and <a href=https://d2v4vjmuxdiocn.cloudfront.net/wp-content/uploads/Curmin-image.jpg>cloudfront.net</a> </p>
+                            </div>
+            
+					
+					
+					
+					
+					
+					
+					
+	<!-- Add Main Content here -->
+                <div class="col-lg-5">
+                        <div class="Title">
+                        <h1>Bobby and Jack's Ingredients</h1> 
+                            <h4>Kohlrabi, Tumeric, and Cilantro!
+                            </h4> 
+                        </div>
+                        <div class="maincontent">    
+                        <p> Welcome to our homepage! This site is so you can learn about three ingredients: Kohlrabi, Tumeric, and Cilantro. The about us link will take you to our page where you can learn all about the owners. If you use the drop down menu you will find links to our pages for each ingredient. If you log in you will beable to make comments on our ingredient pages.</p>
+                      
+                        </div>	
+                </div>
+		</div>
+		</div>
+<?php
+include 'footer.php';
+?>
+
+	</body>
+</html>
